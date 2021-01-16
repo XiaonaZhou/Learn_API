cfb952625e1dd04377ce56f77e138a0ea098545d
Xiaona Zhou <39882924+XiaonaZhou@users.noreply.github.com>
2021-01-16 18:19:41 -0500

    two examples on API

---
+++ GitHub_API.R
@@ -0,0 +1,33 @@
+#install.packages("jsonlite")
+library(jsonlite)
+#install.packages("httpuv")
+library(httpuv)
+#install.packages("httr")
+library(httr)
+
+# Can be github, linkedin etc depending on application
+oauth_endpoints("github")
+
+# Change based on what you 
+myapp <- oauth_app(appname = "First_API",
+                   key = "c6d083e48f0921877068",
+                   secret = "a0a40f39549200ee83ac7b0b27e4ed76790feca6")
+
+# Get OAuth credentials
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+# Use API
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+
+# Take action on http error
+stop_for_status(req)
+
+# Extract content from a request
+json1 = content(req)
+
+# Convert to a data.frame
+gitDF = jsonlite::fromJSON(jsonlite::toJSON(json1))
+
+# Subset data.frame
+gitDF[gitDF$full_name == "jtleek/datasharing", "created_at"] 
